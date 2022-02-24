eb9ca4ecd200ba688605e9350fd9c5bf4daa36bd
VijayaChihnitha <99868333+VijayaChihnitha@users.noreply.github.com>
2022-02-24 19:22:47 +0530

    #include<stdio.h> int main() {     float x,y,sum;     float average;     scanf("%f%f",&x,&y);     sum=x+y;     average=(x+y)/2;     printf("%2.4f",average);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float x,y,sum;
+    float average;
+    scanf("%f%f",&x,&y);
+    sum=x+y;
+    average=(x+y)/2;
+    printf("%2.4f",average);
+    return 0;
+}
